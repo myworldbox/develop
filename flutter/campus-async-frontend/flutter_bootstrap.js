8d1832cf484918177aa400fb50ee2784ae9c70de
thecyangate <56016278+myworldbox@users.noreply.github.com>
2026-03-13 23:21:29 +0800

    bro

--- flutter/campus-async-frontend/flutter_bootstrap.js
+++ flutter/campus-async-frontend/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"80f576e2690e0366991d5dcd648d6cbe1ca464
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "619160679" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3785505215" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
