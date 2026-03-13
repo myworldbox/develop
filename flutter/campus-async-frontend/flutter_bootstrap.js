a461eb8bfd121111d07320d49193f596ae316026
thecyangate <56016278+myworldbox@users.noreply.github.com>
2026-03-13 21:04:48 +0800

    fix

--- flutter/campus-async-frontend/flutter_bootstrap.js
+++ flutter/campus-async-frontend/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187ed6","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"80f576e2690e0366991d5dcd648d6cbe1ca46438","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "887443763" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "619160679" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
