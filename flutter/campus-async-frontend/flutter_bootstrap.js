22697c93a0ed2cbe70f23228a7a2d1fed7f47c96
victor.leung <54nicholasleung45@gmail.com>
2026-03-17 10:34:54 +0800

    fix

--- flutter/campus-async-frontend/flutter_bootstrap.js
+++ flutter/campus-async-frontend/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "363304256" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2190233113" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
