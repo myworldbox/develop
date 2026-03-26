f2d04c6b41a35936c252d1d2870936cc510b7a37
victor.leung <54nicholasleung45@gmail.com>
2026-03-26 12:17:41 +0800

    fix

--- flutter/campus-async/flutter_bootstrap.js
+++ flutter/campus-async/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"f8b46dddd1f36ee24437a95cb7edba8cd5540072","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae12","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1355833375" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3304400077" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
